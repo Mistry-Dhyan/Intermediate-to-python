b9fef3290c79f6512b6c43d473e185929c724edc
Mistry-Dhyan <satyammistry@gmail.com>
2024-05-08 19:49:38 -0400

    Add files via upload

---
+++ 3.1 cw.py
@@ -0,0 +1,38 @@
+use_name = (input('What is your name? : '))
+fl = use_name[0]
+print('The first letter of your name is',fl)
+ll = use_name[len(use_name)-1]
+print('The last letter of your name is',ll)
+
+b = "Blueberry Pancakes!"
+a = 'seahorse'
+print(b[:4])
+print(a[:3])
+print(b[4:10])
+print(a[3:])
+print(b[10:13])
+print(b[:10])
+print(b[13:-1])
+print(b[10:])
+print(b[-1:])
+print(b[-1:9:-1])
+
+cake = "Strawberry Shortcake"
+print(cake[0])
+print(cake[11])
+print(cake[-9])
+print(cake[-20])
+
+use_name = input('What is your name? : ')
+print(use_name[0])
+print(use_name[::-1])
+
+message = "ccowdcionygy nrzuclwepsq!q"
+print(message[0::2])
+
+name = "Justin Timberlake"
+print(name[7:], end='')
+print(name[:6])
+
+secret = "isagbnxiwrptysu ctuueogbyal hlklrae qweoinbkp zusoyY"
+print(secret[::-2])

030dd7e2c3148caa1582ee3cfa4aefb9375259cb
Mistry-Dhyan <satyammistry@gmail.com>
2024-05-13 23:07:58 -0400

    3.1 hw.py

--- 3.1 hw.py
+++ 3.1 hw.py
@@ -1,4 +1,18 @@
 adress = '51 baycliffe cres'
+
 print(adress[3:])
 print(adress[:2])
 
+a = "The Pirates of the Caribbean"
+
+print(a[2])
+print(a[9])
+print(a[17])
+print(a[25])
+print(a[-3])
+print(a[-11])
+print(a[-19])
+print(a[-26])
+
+my_secret_mesage = 'nsfkCsoddjisnhgv ribsd htshhen jbkedsstb jtyhvidnhgu btyoj mitnn dtehiee pwthvojlsen fwrokrrlwdv!h uDood eyiorug ntvhnixnbkf ktrhuaftd zcgoideibnlgg rikse ,tfhier wbjeesbtm ,tthii,nfgs ntrom ydmod?k tHdompfer ayboru4 ndwoy!4!a!bah'
+print(my_secret_mesage[4:-2:2])

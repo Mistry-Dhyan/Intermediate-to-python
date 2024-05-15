b92609cd462fc4acb761862511c4dd25e2f45b36
Mistry-Dhyan <satyammistry@gmail.com>
2024-05-14 20:00:15 -0400

    Add files via upload

---
+++ 3.2 cw.py
@@ -0,0 +1,18 @@
+code = "eg!srtohrsmecvpbixndexg avgrentvioxrsstiesmy mzakh leeukrja'vyuaoovdYc"
+print(code[-2::-3])
+
+print('A method is are the steps you do to something like a code.')
+a = input('What is your name with your name starting with a lowercase. : ')
+b = a . title()
+print(b)
+
+c = a.upper() 
+print(c[0], end='')
+print(a[1::])
+
+num = input("What is your favourite number?")
+while num.isdigit() == False:
+    print('Why would you think that',num,'is a number????!!!!')
+    num = input("What is your favourite number?")
+else:num = int(num)
+print(num,type(num))

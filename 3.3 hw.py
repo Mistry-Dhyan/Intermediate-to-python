0c77a69480b89168bb68d0e19228612d53d9c735
Mistry-Dhyan <satyammistry@gmail.com>
2024-05-22 20:02:04 -0400

    Update 3.3 hw.py

--- 3.3 hw.py
+++ 3.3 hw.py
@@ -1,1 +1,10 @@
-sddd
+a = ['apple , banana , cherry , blueberry']
+while True:
+  for i in a:
+    print(i)
+  p = input('Do you want to modify or add to the list? : ')
+  if p == 'add':
+    d = input('what do you want to add? : ')
+    a = a + [d]
+#   elif p == 'modify':
+#   else: start.begining()

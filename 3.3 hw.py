2b2258013c2f3699beee0acfd1006e2993ba3df3
Mistry-Dhyan <satyammistry@gmail.com>
2024-05-29 18:16:33 -0400

    Update 3.3 hw.py

--- 3.3 hw.py
+++ 3.3 hw.py
@@ -14,3 +14,6 @@ while True:
             b = input('where do you want to insert '+a+' ? : ')
         b = int(b)
         lst[b] = a
+
+##There are many diffrences between lists and strings. One of the ways is that in strings you can't add more carectors but on a list you add stuff to it.
+##Did you know that when you add more carectors to a string then you will get an error but if you add more stuff to a list then you are able to go on without a error.

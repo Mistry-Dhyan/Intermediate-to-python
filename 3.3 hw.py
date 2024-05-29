3d4a0faa6d3e2108f83f3e2e7d4986bb147f88c5
Mistry-Dhyan <satyammistry@gmail.com>
2024-05-28 20:00:28 -0400

    Update 3.3 hw.py

--- 3.3 hw.py
+++ 3.3 hw.py
@@ -1,10 +1,16 @@
-a = ['apple , banana , cherry , blueberry']
+lst = ['apple' , 'banana' , 'cherry' , 'blueberry']
 while True:
-  for i in a:
-    print(i)
-  p = input('Do you want to modify or add to the list? : ')
-  if p == 'add':
-    d = input('what do you want to add? : ')
-    a = a + [d]
-#   elif p == 'modify':
-#   else: start.begining()
+    for i in lst:
+        print(i,end =', ')
+    print()
+    p = input('Do you want to modify or add to the list? : ')
+    if p == 'add':
+        d = input('What do you want to add? : ')
+        lst = lst + [d]
+    elif p == 'modify':
+        a = input('What do you want to insert? : ')
+        b = input('where do you want to insert '+a+' ? : ')
+        while b.isdigit() == False or  int(b) < 0 or int(b) >= len(lst):
+            b = input('where do you want to insert '+a+' ? : ')
+        b = int(b)
+        lst[b] = a

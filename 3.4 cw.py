e7f868f51582f047d657afdd6771290bea786977
Mistry-Dhyan <satyammistry@gmail.com>
2024-06-04 17:38:50 -0400

    Update 3.4 cw.py

--- 3.4 cw.py
+++ 3.4 cw.py
@@ -1,39 +1,39 @@
-# b = [3,5,7,8,9,0]
-# a = b.sort()
-# print(a)
+b = [3,5,7,8,9,0]
+a = b.sort()
+print(a)
 
-# numbers = [3,2,1]
-# numbers.sort()
-# print(numbers) # the list stored in numbers is DIFFERENT now
+numbers = [3,2,1]
+numbers.sort()
+print(numbers) # the list stored in numbers is DIFFERENT now
 
-lst = ['a', 'b', 'c', 'e', 'f']
-lst.append('g') # notice we DO NOT write list =list.append('g')
-print(lst)
+##lst = ['a', 'b', 'c', 'e', 'f']
+##lst.append('g') # notice we DO NOT write list =list.append('g')
+##print(lst)
+##
+##lst.insert(3,'d')# insert item 'd' at index 3
+##print(lst)
+##
+##lst.extend([7,8]) # extend needs a list as input!
+##print(lst)
+##
+##lst.remove('b') # remove the ITEM 'b' from the list
+##print(lst)
+##
+##x = lst.pop(1) # remove item at INDEX 1, and store item in x
+##print(x)
+##print(lst)
+##
+##lst.sort() # now our list is sorted
+##print(lst)
+##lst.reverse() # now our list is reversed
+##print(lst)
 
-lst.insert(3,'d')# insert item 'd' at index 3
-print(lst)
+student = "Liam"
+student.upper()
+print(student)
 
-lst.extend([7,8]) # extend needs a list as input!
-print(lst)
-
-lst.remove('b') # remove the ITEM 'b' from the list
-print(lst)
-
-x = lst.pop(1) # remove item at INDEX 1, and store item in x
-print(x)
-print(lst)
-
-lst.sort() # now our list is sorted
-print(lst)
-lst.reverse() # now our list is reversed
-print(lst)
-
-# student = "Liam"
-# student.upper()
-# print(student)
-
-# student = student.upper()
-# print(student) # now we print "LIAM"!
+student = student.upper()
+print(student) # now we print "LIAM"!
 
 # it will print None
 weekdays = ['monday', 'tuesday']
@@ -51,4 +51,14 @@ animals.insert(2, 'Chameleon')
 
 
 
+animals.sort()
+print(animals[:-2])
+
+for i in range(len(animals)):
+    animals[i] += "!"
 print(animals)
+
+best_animals = animals[:3]
+
+##The most inportat diffrences bettween strings and lists that in strings you can't add more strings in it but you can in a list.
+##mutabitlity is something that is changing.

1dd2cf4ed12517f9836b5799cf7a6cda99c9c5e5
Mistry-Dhyan <satyammistry@gmail.com>
2024-06-04 20:01:46 -0400

    Create list.py

---
+++ list.py
@@ -0,0 +1,38 @@
+lst = []
+print("Do you want to add more words to the list? If yes then what do you want to add the list. If no then write 'done' : ")
+while True:
+    item = input("Enter your Item : ")
+    if item.lower() == 'done':
+        print(lst)
+        break
+    lst.append(item)
+
+while True:
+    edit = input('Do you want to append(add), delete and insert or done or replace? : ')
+    if edit == 'add':
+        appite = input('what do you append(add) you your list? : ')
+        lst.append(appite)
+        print(lst)
+    elif edit == 'delete':
+        popnum = input('what do you want to delete? : ')
+        lst.pop(popnum)
+        print(lst)
+    elif edit == 'insert':
+        numcha = input('What do you want to change? : ')
+        numadd = input('What do you want to change ' +numcha+ ' with? : ')
+        while numadd.isdigit() == False:
+            numadd = input('Which string number do you want to change ' +numcha+ ' with? : ')
+        numadd = int(numadd)
+        lst.insert(numadd,numcha)
+        print(lst)
+    elif edit == 'replace':
+        numcha = input('What do you want to change? : ')
+        numadd = input('Which string number do you want to change ' +numcha+ ' with? : ')
+        while numadd.isdigit() == False:
+            numadd = input('Which string number do you want to change ' +numcha+ ' with? : ')
+        numadd = int(numadd)
+        lst[numadd] = numcha
+        print(lst)
+    elif edit == 'done':
+        print(lst)
+        break

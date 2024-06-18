a3bfc4b04f6335d23416703be73c6b70cf421720
Mistry-Dhyan <satyammistry@gmail.com>
2024-06-18 19:29:13 -0400

    Update 3.6 hw.py

--- 3.6 hw.py
+++ 3.6 hw.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 a = turtle.Turtle()
 a.shape('turtle')
 def draw_rectangle():
@@ -8,9 +9,18 @@ def draw_rectangle():
         a.forward(50)
         a.right(90)
 
+colors =["red", "orange", "yellow", "green", "blue", "cyan", "purple", "pink", "magenta", "light green", "light blue" , "turquoise"]
 def draw_colour_triangle():
-    color = input('Enter the colour you want for your  triangle(red, orange, yellow, green, blue, cyan, purple, pink, magenta, light green, turquoise and light blue(spell the only on color corectly.)) : ')
-    a.color(color)
+    a.color(random.choice(colors))
     for i in range(3):
         a.forward(100)
         a.right(120)
+
+def get_random_colour():
+    a.color(random.choice(colors))
+
+while True:
+    get_random_colour()
+    draw_colour_triangle()
+    a.left(5)
+    draw_rectangle()

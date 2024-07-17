7c2ef6307e8d1e8ea5d8b067a883f7572bfd1e2c
Mistry-Dhyan <satyammistry@gmail.com>
2024-07-17 19:42:10 -0400

    Create 3.8 cw.py

---
+++ 3.8 cw.py
@@ -0,0 +1,23 @@
+my_grocary_list = {'milk' : 4, 'eggs' : 3, 'bread' : 2, 'cake' : 5}
+
+my_empty_dictionary = {}
+
+animals_in_zoo = {}
+animals_in_zoo['zebras'] = 8
+animals_in_zoo['monkeys'] = 12
+animals_in_zoo['dinosurs'] = 0
+print(animals_in_zoo)
+
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+user_ids.update({'theLooper' : 138475, 'stringQueen' : 85739})
+print(user_ids)
+
+oscar_winners = {"Best Picture" : "La La Land", "Best Actor" : "Casey Aeck", "Best Actress" : "Emma Stone", "Animated Feature" : "Zootopia"}
+oscar_winners['Supporting Actress'] = 'Viola Davis'
+oscar_winners['Best Picture'] = 'Moonlight'
+print(oscar_winners)
+
+students = {'Jenny': 61, 'Alexus': 70, 'Sam': 67, 'Grace': 64}
+students['james'] = 77
+students['Sam'] = 66
+print(students)

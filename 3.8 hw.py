a6b853ee5ff5969ca891ecb1933915548659a19b
Mistry-Dhyan <satyammistry@gmail.com>
2024-07-17 19:58:16 -0400

    Create 3.8 hw.py

---
+++ 3.8 hw.py
@@ -0,0 +1,23 @@
+Price_and_stuff_in_my_store = {'coffee' : 2, 'bagel' : 3, 'dounut' : 1, 'sandwhich' : 5}
+
+empty_dict = {}
+
+sports_to_play = {}
+sports_to_play['soccer'] = 7
+sports_to_play['rugby'] = 9
+sports_to_play['basketball'] = 11
+print(sports_to_play)
+
+user_ids = {"brianDog": 9018293, "jennyCat": 119238}
+user_ids.update({'cuteDuck' : 178375, 'sweetPie' : 92537})
+print(user_ids)
+
+movies = {"Best Picture": "Mickey Mouse", "Best Actor": "Mickey", "Best Actress": "Minnie", "Animated Feature": "Mouse"}
+movies['Supporting Actress'] = 'Anyone'
+movies['Best Picture'] = 'Frozen'
+print(movies)
+
+fruits = {'apple': 6, 'strawberry': 7, 'watermelon': 3, 'pineapple': 4}
+fruits['orange'] = 7
+fruits['watermelon'] = 5
+print(fruits)
